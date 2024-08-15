7ea73e5e51b2fdf32c454846a311bf3a2ebee8e5
Veena Suresh <veenasuresh28487@gmail.com>
2024-08-15 17:55:35 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import TicTacToe from './Components/tictactoe/TicTacToe'
+
+function App() {
+
+  return (
+    <>
+      <div>
+    <TicTacToe/>
+        </div>
+    </>
+  )
+}
+
+export default App
